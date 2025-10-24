3f4f5f8f0045ae7d1ff2617015b74c502fb68461
Shubham Jha <shubham_cse_2026@msit.in>
2025-10-24 08:33:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9028376727-f5207",
+  "appId": "1:89731176071:web:1f603ff54b60122982ca22",
+  "apiKey": "AIzaSyBcvo9cmA5trtVf0teBWfAFcIGx_eYH2eU",
+  "authDomain": "studio-9028376727-f5207.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "89731176071"
+};
